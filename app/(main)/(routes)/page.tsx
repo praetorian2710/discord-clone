7ebe92fbe822cf78cbee200488a890a0ced1b8b0
praetorian2710 <joti.ban.2710@gmail.com>
2023-09-30 22:09:09 +0530

    Started database

--- app/(main)/(routes)/page.tsx
+++ app/(main)/(routes)/page.tsx
@@ -1,3 +1,4 @@
+import { ModeToggle } from "@/components/mode-toggle";
 import { Button } from "@/components/ui/button";
 import { UserButton } from "@clerk/nextjs/app-beta";
 export default function Home() {
@@ -5,6 +6,7 @@ export default function Home() {
     <div className="">
       {/* provides with the signout facility button and the route which needs to be redirected to after signout */}
       <UserButton afterSignOutUrl="/" />
+      <ModeToggle />
     </div>
   );
 }
